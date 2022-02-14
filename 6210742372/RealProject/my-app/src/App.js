e39f903adb6829ebb59abb3c99810a9b184ffd5d
Domeไงจะใครล่ะ <60128308+dome44-tp@users.noreply.github.com>
2022-02-14 17:33:33 +0700

    Topbar Animation

--- 6210742372/RealProject/my-app/src/App.js
+++ 6210742372/RealProject/my-app/src/App.js
@@ -4,16 +4,18 @@ import Portfolio from './components/portfolio/Portfolio';
 import Work from './components/work/Work';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
+import { useState } from 'react';
 import './App.css';
 
 
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="App">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className='sections'>
-        ghghhgh
+        .
         <Intro />
         <Portfolio />
         <Work />
--- 6210742372/RealProject/my-app/src/components/topbar/Topbar.js
+++ 6210742372/RealProject/my-app/src/components/topbar/Topbar.js
@@ -1,17 +1,27 @@
 import "./topbar.css"
+import {Person, Mail} from "@material-ui/icons"
 
-const Topbar = () => {
+const Topbar = ({menuOpen, setMenuOpen}) => {
     return (
-        <div className='topbar'>
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">Portfolio</a>
                     <div className="itemContainer">
-
+                        <Person className="icon"/>
+                        <span className="spanData">085 589 4133</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span className="spanData">thanapon.workdev@gmail.com</span>
                     </div>
                 </div>
                 <div className="right">
-                    im right
+                    <div className="hamburger" onClick = {()=>setMenuOpen(!menuOpen)}>
+                        <span className="one"></span>
+                        <span className="two"></span>
+                        <span className="three"></span>
+                    </div>
                 </div>
             </div>
         </div>
