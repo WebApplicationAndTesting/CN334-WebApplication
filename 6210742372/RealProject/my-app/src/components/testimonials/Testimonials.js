b71c20f3ab52d042de03d1186ab6201e12af125e
Domeไงจะใครล่ะ <60128308+dome44-tp@users.noreply.github.com>
2022-02-15 21:48:09 +0700

    Build Structure testimonial page

--- 6210742372/RealProject/my-app/src/components/testimonials/Testimonials.js
+++ 6210742372/RealProject/my-app/src/components/testimonials/Testimonials.js
@@ -4,7 +4,24 @@ import "./testimonials.css"
 const Testimonials = () => {
     return (
         <div className='testimonials' id="testimonials">
-            TESTIMONIALS
+            <h1>TESTIMONIALS</h1>
+            <div className="container">
+                <div className="card">
+                    <div className="top">
+                        <img src="asset/right-arrow.png" className="left" alt="" />
+                        <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930" alt="" />
+                        <img src="asset/youtube.png" alt="" />
+                    </div>
+                    <div className="center">
+                        Thanapon 6210742372 Thanapon 6210742372 
+                        Thanapon 6210742372 Thanapon 6210742372
+                    </div>
+                    <div className="bottom">
+                        <h3>Dome</h3>
+                        <h4>CEO</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
