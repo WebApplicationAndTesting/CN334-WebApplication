e9712b5563cf2a82b5b05d7d5cb00826a79f6437
Domeไงจะใครล่ะ <60128308+dome44-tp@users.noreply.github.com>
2022-02-23 13:01:24 +0700

    [ADD] Usertest.php, a_user_has_phone

--- 6210742372/6210742372_Unit/tests/Unit/UserTest.php
+++ 6210742372/6210742372_Unit/tests/Unit/UserTest.php
@@ -2,7 +2,8 @@
 
 namespace Tests\Unit;
 
-
+use App\Phone;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Schema;
@@ -21,4 +22,14 @@ class UserTest extends TestCase
           'id','name', 'email', 'email_verified_at', 'password'
       ]), 1);
   }
+
+  public function a_user_has_a_phone()  {
+      $user = factory(User::class)->create(); 
+      $phone = factory(Phone::class)->create(['user_id' => $user->id]); 
+      // Method 1:
+      $this->assertInstanceOf(Phone::class, $user->phone); 
+      // Method 2:
+      $this->assertEquals(1, $user->phone->count()); 
+  }
+
 }
